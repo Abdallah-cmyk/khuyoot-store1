35b9adee00926309fdab5d345dcefbe3e3f0f5e5
Abdullah <abdullahhamad010.00@gmail.com>
2026-07-30 19:56:25 +0300

    Add CartProvider to main.jsx
    Wrapped the App component with CartProvider for context.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // 1. أضفنا استيراد الموجه هنا
-import App from './App.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { CartProvider } from './CartContext.jsx'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* 2. قمنا بتغليف المكون App هنا */}
-      <App />
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
